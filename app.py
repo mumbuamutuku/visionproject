9cd1b4ef5860e70633614b5bf9c019edb88fe109
mumbuamutuku <mumbuamutuku@gmail.com>
2025-05-03 13:20:50 +0300

    added waittress

--- app.py
+++ app.py
@@ -219,6 +219,3 @@ def api_classify():
         return jsonify(result)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
